5c721d6fa613e52c379daec76cc73f2e10544fcd
HoangDien17 <hoangkhaidien93@gmail.com>
2021-06-28 17:15:59 +0700

    api_send_email

--- src/controllers/FormController.js
+++ src/controllers/FormController.js
@@ -1,8 +1,61 @@
 const FormService = require('../services/formService');
 
 let apiCreateForm = async (req, res) => {
-  const {statusCode, message, data} = await FormService.apiCreateForm(req.body);
+  const {statusCode, message, data} = await FormService.apiCreateForm(req.body, req.payload.id);
   return res.status(statusCode).send({statusCode, message, data});
 };
 
-module.exports = { apiCreateForm }
+let apiGetFormMySelf = async(req, res) => {
+  const {statusCode, message, data} = await FormService.apiGetFormMySelf(req.payload.id);
+  return res.status(statusCode).send({statusCode, message, data});
+};
+
+let apiGetFormSubordinates = async (req, res) => {
+  let page = req.query.page;
+  const limit = 5;
+  const {statusCode, message, data} = await FormService.apiGetFormSubordinates(req.payload.id, limit, page);
+  return res.status(statusCode).send({statusCode, message, data});
+};
+
+let apiGetFormAll = async (req, res) => {
+  let page = req.query.page;
+  const limit = 5;
+  const {statusCode, message, data} = await FormService.apiGetFormAll(limit, page);
+  return res.status(statusCode).send({statusCode, message, data});
+};
+
+let apiSubmittedForm = async (req, res) => {
+  const {statusCode, message, data} = await FormService.apiSubmittedForm(req.body, req.payload.id); // req.body: content và typeform
+  return res.status(statusCode).send({statusCode, message, data});
+}
+
+let apiPendingApprovalForm = async (req, res) => {
+  const {statusCode, message, data} = await FormService.apiPendingApprovalForm(req.body.id, req.payload.id); 
+  return res.status(statusCode).send({statusCode, message, data});
+};
+
+let apiApproveForm = async (req, res) => {
+  const {statusCode, message, data} = await FormService.apiApproveForm(req.body, req.payload.id); 
+  return res.status(statusCode).send({statusCode, message, data});
+};
+
+let apiConfirmFormByHr = async (req, res) => {
+  const {statusCode, message, data} = await FormService.apiConfirmFormByHr(req.body.id); 
+  return res.status(statusCode).send({statusCode, message, data});
+};
+
+let apiGetUndoneProbationary = async (req, res) => {
+  let page = req.query.page;
+  const limit = 5;
+  const {statusCode, message, data} = await FormService.apiGetUndoneProbationary(limit, page); 
+  return res.status(statusCode).send({statusCode, message, data});
+}
+
+let apiGetUndoneAssessment = async (req, res) => {
+  let page = req.query.page;
+  const limit = 5;
+  const {statusCode, message, data} = await FormService.apiGetUndoneAssessment(); 
+  return res.status(statusCode).send({statusCode, message, data});
+}
+
+module.exports = { apiCreateForm, apiGetFormMySelf, apiGetFormSubordinates, apiGetFormAll, apiPendingApprovalForm, apiSubmittedForm, apiApproveForm, apiConfirmFormByHr, apiGetUndoneProbationary, apiGetUndoneAssessment };
--- src/routes/api-form-route.js
+++ src/routes/api-form-route.js
@@ -8,4 +8,13 @@ const AuthenToken = require('../middlewares/authenToken');
 module.exports = (fix) => {
   fix.use("/forms", AuthenToken, router);
   router.post("/", Authorization.Auth(['Admin', 'Hr']), FormController.apiCreateForm);
+  router.get("/", FormController.apiGetFormMySelf);
+  router.get("/subordinates", FormController.apiGetFormSubordinates);
+  router.get("/all", Authorization.Auth(['Hr']), FormController.apiGetFormAll);
+  router.put("/", FormController.apiSubmittedForm);
+  router.put("/pending", FormController.apiPendingApprovalForm);
+  router.put("/approve", FormController.apiApproveForm);
+  router.put("/confirm", Authorization.Auth(['Hr']), FormController.apiConfirmFormByHr);
+  router.post("/undone/probationary", Authorization.Auth(['Hr']), FormController.apiGetUndoneProbationary);
+  router.post("/undone/Assessment", Authorization.Auth(['Hr']), FormController.apiGetUndoneAssessment);
 }
